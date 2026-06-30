8fe581e60bd4d36af7e982e0d8ab680460d8cd51
fhilip <fhilip.wang1@gmail.com>
2026-06-30 16:20:21 +0700

    Config: database
    pengerjaan environmenet dan pergantian model ke sequelize

---
+++ src/config/db.js
@@ -0,0 +1,29 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config({path: path.resolve(__dirname, "../../.env")});
+console.log(__dirname)
+console.log(process.env.DB_NAME)
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  },
+);
+
+try {
+  await sequelize.authenticate();
+  console.log("Koneksi Database Berhasil...");
+} catch (error) {
+  console.log("Koneksi Database Gagal : ", error);
+}
