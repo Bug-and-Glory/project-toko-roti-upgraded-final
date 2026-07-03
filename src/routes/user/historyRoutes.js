9b160d7674a32c64597a6d6f2a63e602296f3511
Muhadist As-Shidiq Olii <muhadist.as-shidiq@students.mikroskil.ac.id>
2026-07-03 18:53:45 +0700

    fix history
    fix history

--- src/routes/user/historyRoutes.js
+++ src/routes/user/historyRoutes.js
@@ -1,19 +1,10 @@
-// import express from "express";
-// import * as historyController from "../../controllers/user/historyController.js";
-
-// const router = express.Router();
-
-// router.get("/history", historyController.getHistory);
-
-// export default router;
-
-
 import express from "express";
 import * as historyController from "../../controllers/user/historyController.js";
 
 const router = express.Router();
 
-router.get("/history", historyController.getAllHistory);
-router.get("/history/:id", historyController.getHistoryById);
+router.get("/history", historyController.showHistoryPage);
+router.get("/api/history", historyController.getAllHistory);
+router.get("/api/history/:id", historyController.getHistoryById);
 
 export default router;
