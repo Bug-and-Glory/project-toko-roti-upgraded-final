dbbdc406435506bc3be7c9247a0c7b009fe48ada
Muhadist As-Shidiq Olii <muhadist.as-shidiq@students.mikroskil.ac.id>
2026-07-04 00:00:04 +0700

    fix index
    fix home controller n index ejs

--- src/controllers/user/homeController.js
+++ src/controllers/user/homeController.js
@@ -1,9 +1,45 @@
+import { Op } from "sequelize";
+import {
+  Product,
+  ProductDetail,
+  SubCategory,
+  MasterCategory,
+} from "../../models/index.js";
+
+// Nama produk yang mau ditampilin di section "Menu Pilihan" home page
+const HOME_MENU_TARGETS = [
+  "Birthday cake",
+  "Caramel Macchiato",
+  "Macaron",
+  "Strawberry Danish",
+  "Cookies",
+  "Strawberry milk",
+].map((name) => name.toLowerCase());
+
+// Render halaman home + produk pilihan, data langsung dikirim ke view (server-side)
 export const index = async (req, res, next) => {
   try {
-    res.render("user/index", {
-      title: "Home",
-      user: req.session.user || null,
+    const products = await Product.findAll({
+      where: {
+        name: { [Op.in]: HOME_MENU_TARGETS.map((n) => n) },
+      },
+      include: [
+        {
+          model: SubCategory,
+          as: "Product_SubCategory",
+          attributes: ["id", "name"],
+          include: [
+            {
+              model: MasterCategory,
+              as: "Sub_MasterCategory",
+              attributes: ["id", "name"],
+            },
+          ],
+        },
+      ],
     });
+
+    res.render("user/index", { title: "Home", products });
   } catch (error) {
     next(error);
   }
