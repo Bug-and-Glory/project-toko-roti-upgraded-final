260d4a0f282746d8271ecd35d0b91e960328a74c
fhilip <fhilip.wang1@gmail.com>
2026-07-03 17:19:43 +0700

    feat: login/ logout
    Feature logout
    perubahan index.ejs

--- src/controllers/user/homeController.js
+++ src/controllers/user/homeController.js
@@ -2,6 +2,7 @@ export const index = async (req, res, next) => {
   try {
     res.render("user/index", {
       title: "Home",
+      user: req.session.user || null,
     });
   } catch (error) {
     next(error);
