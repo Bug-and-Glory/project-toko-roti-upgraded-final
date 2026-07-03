c922628d3cde3bf56d59fca91fa54f3b723446eb
sitizaharaa <sitizahara0305@gmail.com>
2026-07-03 15:57:13 +0700

    nambahin store route

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -6,7 +6,8 @@ import orderRoutes from "./orderRoutes.js";
 import historyRoutes from "./historyRoutes.js";
 import contactRoutes from "./contactRoutes.js";
 import commentRoutes from "./commentRoutes.js";
-import aboutRoutes from "./aboutRoutes.js"
+import aboutRoutes from "./aboutRoutes.js";
+import storeRoutes from "./storeRoutes.js";
 
 const router = express.Router();
 
@@ -18,5 +19,6 @@ router.use("/", historyRoutes);
 router.use("/", contactRoutes);
 router.use("/", commentRoutes);
 router.use("/", aboutRoutes);
+router.use("/", storeRoutes);
 
 export default router;
--- src/routes/user/storeRoutes.js
+++ src/routes/user/storeRoutes.js
@@ -3,8 +3,8 @@ import express from "express"
 const router = express.Router()
 
 
-router.get("/aboutUs", (req, res) =>{
-    return res.render("user/about_us")
+router.get("/store", (req, res) =>{
+    return res.render("user/store")
 })
 
 export default router;
